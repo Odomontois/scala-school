21eb75cc16c860d8c962da2d2ec3deab19df8cc4
LazyNekogami <redgarwind@gmail.com>
2017-10-12 17:01:48 +0300

    Added part of 4th assignment (lectures.oop.BST)

--- src/main/scala/lectures/oop/BST.scala
+++ src/main/scala/lectures/oop/BST.scala
@@ -1,5 +1,8 @@
 package lectures.oop
 
+import scala.collection.immutable.Queue
+import scala.util.Random
+
 
 /**
   * BSTImpl - это бинарное дерево поиска, содержащее только значения типа Int
@@ -32,16 +35,106 @@ trait BST {
   def find(value: Int): Option[BST]
 }
 
+
 case class BSTImpl(value: Int,
-                   left: Option[BSTImpl] = None,
-                   right: Option[BSTImpl] = None) extends BST {
+                   left: Option[BST] = None,
+                   right: Option[BST] = None) extends BST {
 
-  def add(newValue: Int): BST = ???
+  def add(newValue: Int): BST = {
+    def addToChild(child: Option[BST]): Option[BST] = {
+      val newChild = child match {
+        case Some(bst) => bst.add(newValue) //.asInstanceOf[BSTImpl]
+        case None => BSTImpl(newValue)
+      }
+      Option(newChild)
+    }
 
-  def find(value: Int): Option[BST] = ???
+    if (newValue == value) this
+    else if (newValue < value) {
+      val newLeft = addToChild(left)
+      BSTImpl(value, newLeft, right)
+    } else {
+      val newRight = addToChild(right)
+      BSTImpl(value, left, newRight)
+    }
+  }
 
-  // override def toString() = ???
+  def find(value: Int): Option[BST] = {
+    if (value == this.value) Option(this)
+    else if (value < this.value) {
+      if (left.nonEmpty) left.get.find(value)
+      else None
+    } else {
+      if (right.nonEmpty) right.get.find(value)
+      else None
+    }
+  }
 
+  lazy val depth: Int = {
+    def childDepth(child: Option[BST]): Int = child match {
+      case Some(bst) => math.max(childDepth(bst.left), childDepth(bst.right)) + 1
+      case None => 0
+    }
+
+    math.max(childDepth(left), childDepth(right)) + 1
+  }
+
+
+  // TODO: Please refactor it, Someone! T___T
+  override def toString() = {
+    def toString(acc: String)(queue: Queue[(Option[BST], Int, Boolean)]): String = {
+      if (queue.isEmpty) acc
+      else {
+        val ((headMaybe, headLevel, isLeft), queueTail) = queue.dequeue
+        val newQueue = if (headLevel >= 1) {
+          val childrenLevel = headLevel - 1
+          val (maybeLeft, maybeRight) = headMaybe match {
+            case Some(bts) => (bts.left, bts.right)
+            case None => (None, None)
+          }
+          queueTail
+            .enqueue((maybeLeft, childrenLevel, true))
+            .enqueue((maybeRight, childrenLevel, false))
+        } else Queue.empty[(Option[BST], Int, Boolean)]
+        val newString_? =
+          if (newQueue.nonEmpty && headLevel != newQueue.head._2) "\n"
+          else ""
+        val newAcc = acc + atLevel(headLevel, headMaybe, isLeft) + newString_?
+        toString(newAcc)(newQueue)
+      }
+    }
+
+    /**
+      * @param level root has level equal to depth() - 1, and deepest leaf - equal to 1
+      */
+    def atLevel(level: Int, child: Option[BST], isLeft: Boolean): String = {
+      val margin = Array.fill[Char](1 << level)(' ').mkString("")
+      val halfMargin = Array.fill[Char](1 << level - 1)(' ').mkString("")
+      child match {
+        case Some(bts) if isLeft =>  s"$margin ${bts.value}"
+        case Some(bts) if !isLeft =>  s"$halfMargin ${bts.value}"
+        case None => s"$margin $margin"
+      }
+    }
+
+    toString("")(Queue((Option(this), depth - 1, true)))
+  }
+}
+
+object BSTImplGenerator {
+  val rnd = new Random()
+
+  def apply(nodesCount: Int, bound: (Int, Int) = (0, Int.MaxValue)): Option[BSTImpl] = {
+    if (nodesCount == 0) None
+    else {
+      val leftSize = rnd.nextInt(nodesCount / 2 + 1) + nodesCount / 4
+      val rightSize = nodesCount - leftSize - 1
+      val value = math.max(rnd.nextInt(bound._2 - bound._1), leftSize + 1) + bound._1
+      val left = BSTImplGenerator(leftSize, (bound._1, value))
+      val right = BSTImplGenerator(rightSize, (value + 1, bound._2))
+      Option(BSTImpl(value, left, right))
+    }
+  }
 }
 
 object TreeTest extends App {
@@ -56,8 +149,10 @@ object TreeTest extends App {
 
   // Generate huge tree
   val root: BST = BSTImpl(maxValue / 2)
-  val tree: BST = ??? // generator goes here
+  val tree: BST = BSTImplGenerator(nodesCount, (0, maxValue)).getOrElse(BSTImpl(-1)) // generator goes here
 
+  println(s"original depth = ${tree.asInstanceOf[BSTImpl].depth}")
+  println(s"original tree =\n$tree")
   // add marker items
   val testTree = tree.add(markerItem).add(markerItem2).add(markerItem3)
 
@@ -66,5 +161,5 @@ object TreeTest extends App {
   require(testTree.find(markerItem).isDefined)
   require(testTree.find(markerItem).isDefined)
 
-  println(testTree)
+  println(s"testTree =\n$testTree")
 }
